0284dd0e6034917a20c87df9a5aea2f3b1de4b79
kimbogyu <bg0820@naver.com>
2020-04-13 18:39:00 +0900

    추가되지 않은 LecturePage 라우터 제거

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import MainPage from "./Pages/Main";
 import LoginPage from "./Pages/Login";
-import LecturePage from "./Pages/Lecture";
 
 import "./index.scss";
 
@@ -12,9 +11,8 @@ const App = (props) => {
         <React.Fragment>
             <BrowserRouter>
                 <Switch>
-                    <Route exact path="/" component={MainPage} /> //
+                    <Route exact path="/" component={MainPage} />
                     <Route exact path="/login" component={LoginPage} />
-                    <Route exact path="/lecture" component={LecturePage} />
                 </Switch>
             </BrowserRouter>
         </React.Fragment>
