8882ac60d4a454d9e47c1e4a037534ed50ade615
kimbogyu <bg0820@naver.com>
2020-04-16 17:24:52 +0900

    #5 회원가입 화면 로그인 화면 디자인 통일화

--- src/Pages/Register/index.jsx
+++ src/Pages/Register/index.jsx
@@ -6,33 +6,56 @@ import MainLayout from "@templates/MainLayout";
 
 import "./style.scss";
 
-const RegisterPage = props => {
+const RegisterPage = (props) => {
     return (
-        <div className="MainLayout">
-            <div className="MainBox">
-                <p className="title">회원가입</p>
+        <MainLayout>
+            <div className="RegisterPage">
+                <div className="Register">
+                    <p className="title">회원가입</p>
 
-                <div className="inputs">
-                <Input type="text" placeholder="아이디" margin="0px 0px 10px 0px" width="100%"></Input>
-                
-                <Input type="password" placeholder="비밀번호" margin="0px 0px 10px 0px" width="100%"></Input>
-              
-                <Input type="password" placeholder="비밀번호 확인" margin="0px 0px 10px 0px" width="100%"></Input>
-               
-                <Input type="email" placeholder="이메일" margin="0px 0px 10px 0px" width="100%"></Input>
-                <Input type="text" placeholder="OTP코드" margin="0px 0px 10px 0px" width="100%"></Input>
+                    <div className="inputs">
+                        <Input
+                            type="text"
+                            placeholder="아이디"
+                            margin="0px 0px 10px 0px"
+                            width="100%"
+                        ></Input>
 
+                        <Input
+                            type="password"
+                            placeholder="비밀번호"
+                            margin="0px 0px 10px 0px"
+                            width="100%"
+                        ></Input>
+
+                        <Input
+                            type="password"
+                            placeholder="비밀번호 확인"
+                            margin="0px 0px 10px 0px"
+                            width="100%"
+                        ></Input>
+
+                        <Input
+                            type="email"
+                            placeholder="이메일"
+                            margin="0px 0px 10px 0px"
+                            width="100%"
+                        ></Input>
+                        <Input
+                            type="text"
+                            placeholder="OTP코드"
+                            margin="0px 0px 10px 0px"
+                            width="100%"
+                        ></Input>
+                    </div>
+
+                    <div className="boxBottom">
+                        <p className="error">에러메세지 출력</p>
+                        <Button value="회원가입" height="50px"></Button>
+                    </div>
                 </div>
-
-                <div className="boxBottom">
-
-                <p className="error">에러메세지 출력</p>
-                <Button color="blue" value="회원가입"></Button>
-
-                </div>
-               
             </div>
-        </div>
+        </MainLayout>
     );
 };
 
