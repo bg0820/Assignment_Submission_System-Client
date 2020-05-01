07db5c2a08d295fac9b12c4a44e3e9835109c12c
kimbogyu <bg0820@naver.com>
2020-05-02 04:47:29 +0900

    #32  로그인 서버 연동 및 로그인 안되어 있을시에 로그인 페이지로 이동, 로그인 디자인 수정

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState, useRef, memo } from "react";
+import { observer, inject } from "mobx-react";
+import { Link } from 'react-router-dom';
 
 import Input from "@components/Input";
 import Button from "@components/Button";
 import MainLayout from "@templates/MainLayout";
 
-import Main from "../../Store/Main";
 import * as Util from "@util";
 import "./style.scss";
 
 const LoginPage = (props) => {
+    const {storeMain} = props;
     const [id, setId] = useState("");
     const [pw, setPw] = useState("");
 
@@ -19,14 +21,20 @@ const LoginPage = (props) => {
     const handlePwChange = (e) => {
         setPw(e.target.value);
     };
+
     const loginBtn = (e) => {
         Util.requestServer("auth/login", "POST", {
             id: id,
             pw: pw,
-        }).then(function (result) {
-            console.log(result);
+        }).then(async function (result) {
             if (result.code == 200) {
+                sessionStorage['token'] = result.body.token;
+
+                let resp = await Util.requestServer('auth/info', 'get', {});
+                storeMain.login(resp.body.info.id, resp.body.info.name, resp.body.info.userType);
                 alert(result.body.msg);
+
+                props.history.push('/');
             } else {
                 alert(result.body.msg);
             }
@@ -40,43 +48,34 @@ const LoginPage = (props) => {
                     <p className="Title"> 성공회대학교 과제 제출 시스템 </p>
 
                     <div className="InfoInput">
-                        <ul>
-                            <li>
-                                <Input
-                                    type="text"
-                                    className="Id"
-                                    placeholder="아이디를 입력하세요."
-                                    padding="0px 0px 3px 0px"
-                                    value={id}
-                                    onChange={handleIdChange}
+                        <div className="wrap">
+                            <Input
+                                type="text"
+                                className="Id"
+                                placeholder="아이디를 입력하세요."
+                                value={id}
+                                onChange={handleIdChange}
+                                margin="0px 0px 10px 0px"
                                 />
-                            </li>
-                            <li>
-                                <Input
-                                    type="password"
-                                    className="Password"
-                                    placeholder="비밀번호를 입력하세요."
-                                    padding="0px 0px 3px 0px"
-                                    value={pw}
-                                    onChange={handlePwChange}
+                            <Input
+                                type="password"
+                                className="Password"
+                                placeholder="비밀번호를 입력하세요."
+                                value={pw}
+                                onChange={handlePwChange}
+                                margin="0px 0px 10px 0px"
                                 />
-                            </li>
-                        </ul>
+                        </div>
                     </div>
 
-                    <div className="Bottom">
-                        <input
-                            type="button"
-                            className="IdPwButton"
-                            value="아이디/비밀번호 찾기"
-                        />
+                    <div className="Bottom">                        
+                        <div className="findDirect">
+                            <Link to="/register">아이디/비밀번호 찾기</Link>
+                        </div>
 
-                        <input
-                            type="button"
-                            className="EnrollButton"
-                            value="학생 계정 등록"
-                        />
-
+                        <div className="regDirect">
+                            <Link to="/register">학생 계정 등록</Link>
+                        </div>
                         <Button
                             value="로그인"
                             height="50px"
@@ -89,4 +88,6 @@ const LoginPage = (props) => {
     );
 };
 
-export default LoginPage;
+export default inject(
+    "storeMain"
+)(memo(LoginPage));
