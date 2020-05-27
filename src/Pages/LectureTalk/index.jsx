4010131aedfc8f67c390683839e14616db5ebdde
hyunju <aphj2309@gmail.com>
2020-05-27 14:43:57 +0900

    #11 강의톡 수정 완료

--- src/Pages/LectureTalk/index.jsx
+++ src/Pages/LectureTalk/index.jsx
@@ -2,15 +2,24 @@ import React, { useEffect, useState, useRef, memo } from "react";
 
 import MainLayout from "@templates/MainLayout";
 import TalkLayout from "@templates/TalkLayout";
-
+import BubbleChat from "@components/BubbleChat";
 
 import "./style.scss";
 
 const LectureTalk = (props) => {
     return (
-    <MainLayout>
-        <TalkLayout title="강의 톡"></TalkLayout>
-    </MainLayout>);
+        <MainLayout>
+            <TalkLayout title="강의 톡">
+                        <BubbleChat>
+
+                        </BubbleChat>
+                    
+                        <BubbleChat color="green">
+
+                        </BubbleChat>
+
+            </TalkLayout>
+        </MainLayout>);
 }
 
 export default LectureTalk;
