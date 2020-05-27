b978fd92380c879656ee844d4a92962b2e3bfa57
hyunju <aphj2309@gmail.com>
2020-05-27 14:43:47 +0900

    #11 채팅 템플릿 수정 완료

--- src/Templates/TalkLayout/index.jsx
+++ src/Templates/TalkLayout/index.jsx
@@ -9,7 +9,7 @@ const TalkLayout = (props) => {
     const [title, setTitle] = useState("");
 
     const inputControl = () => {
-        if(props.title.indexOf("공지") == 1) {
+        if (props.title.indexOf("공지") == 1) {
             //userType이 교수면 input 사용가능
             // 학생이면 input에 disable 넣기
         }
@@ -19,20 +19,17 @@ const TalkLayout = (props) => {
     };
 
     const btnClick = (e) => {
-        
+
     };
 
     return (
         <div className="TalkLayout">
             <p className="title">{props.title}</p>
+
             <div className="talk">
-                <div className="leftTalk">
-                    
-                </div>
-                <div className="rightTalk">
-                    
-                </div>
+                {props.children}
             </div>
+
             <div className="bottom">
                 <Input value={title} onChange={handleTitleChange} placeholder="내용을 입력하세요." height="40px" margin="0px 10px 0px 0px"></Input>
                 <Button onClick={btnClick} value="전송" width="110px" height="40px" margin="10px 0px 0px 0px"></Button>
