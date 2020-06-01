8158376214d6bcb209d76aca79d997b476bf46ea
hyeonji <hyeonji.27@daum.net>
2020-06-01 15:35:52 +0900

    #46 과제 박스 디자인 수정

--- src/Components/AssignmentCodeBox/index.jsx
+++ src/Components/AssignmentCodeBox/index.jsx
@@ -2,22 +2,26 @@ import React, { useEffect, useState, useRef, memo } from "react";
 import { observer, inject } from "mobx-react";
 
 import Button from "@components/Button";
+import Input from "@components/Input";
 
 import * as Util from "@util";
 import "./style.scss";
 
 const AssignmentCodeBox = (props) => {
     const { storeMain, storeModal, storeLecture } = props;
+    const [score, setScore] = useState("");
 
     const handleEvaluation = (e) => {
-        storeModal.modalCall({
-            modalView: "evaluateAssignment",
-            modalTitle: "과제 평가",
-        });
+        
+    };
+
+    const handleScoreChange = (e) => {
+        setScore(e.target.value);
     };
 
     return(
         <div className="AssignmentCodeBox">
+        <div className="box">
             <div className="left">
                 <div className="studentInfo">
                     <p className="studentNumber">201734001</p>
@@ -28,6 +32,15 @@ const AssignmentCodeBox = (props) => {
                 </div>
             </div>
             <div className="right">
+                <Input
+                    placeholder="점수"
+                    width="45px"
+                    height="small"
+                    margin="0px 15px 0px 0px"
+                    value={score}
+                    onChange={handleScoreChange}
+                />
+
                 <Button
                     value="평가"
                     width="90px"
@@ -38,6 +51,7 @@ const AssignmentCodeBox = (props) => {
 
                 </div>
             </div>
+        </div>
         </div>
     );
 }
