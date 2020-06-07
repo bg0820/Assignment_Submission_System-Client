af3bbe3f109eba7393eb12c5fc741c02cfc957b5
hyeonji <hyeonji.27@daum.net>
2020-06-07 20:01:30 +0900

    #24 제출된 과제 서버연동 및 평가 구현

--- src/Pages/AssignmentEvaluation/index.jsx
+++ src/Pages/AssignmentEvaluation/index.jsx
@@ -1,24 +1,93 @@
 import React, { useEffect, useState, useRef, memo } from "react";
+import { observer, inject } from "mobx-react";
 
 import MainLayout from "@templates/MainLayout";
 import AssignmentCodeBox from "@components/AssignmentCodeBox";
 import AssignmentHtmlBox from "@components/AssignmentHtmlBox";
 
+import * as Util from "@util";
 import "./style.scss";
 
 const AssignmentEvaluation = (props) => {
     const { storeMain, storeLecture, storeTask } = props;
     const [list, setList] = useState([]);
+    const [language, setLanguage] = useState();
+    const [assignment, setAssignment] = useState({
+        id: "",
+        studentName: "",
+        score: "",
+        code: "",
+        result: "",
+        evaluationIdx: "",
+    });
+
     let assignmentElem = null;
 
-    if(props.language == "HTML") {
+    useEffect(() => {
+        Util.requestServer("task/list/apply", "GET", {
+            taskIdx: props.match.params.id,
+        }).then(function(resp) {
+            let body = resp.body;
+
+            if(resp.code === 200) {
+                setList(body.list);
+                //setAssignment(body.assignment);
+                setLanguage(body.language);
+            }
+        })
+    }, []);
+
+
+    if(language == "HTML") {
+        assignmentElem = list.map((item, idx) => {
+            return(
+                <AssignmentHtmlBox
+                    id={item.id}
+                    studentName={item.studentName}
+                    score={item.score}
+                    code={item.code}
+                    result={item.result}
+                    evaluationIdx={item.evaluationIdx}
+                ></AssignmentHtmlBox>
+            );
+        });
+        /*
         assignmentElem = (
-            <AssignmentHtmlBox></AssignmentHtmlBox>
+            <AssignmentHtmlBox
+                id={assignment.id}
+                studentName={assignment.studentName}
+                score={assignment.score}
+                code={assignment.code}
+                result={assignment.result}
+                evaluationIdx={assignment.evaluationIdx}
+            ></AssignmentHtmlBox>
         );
+        */
     } else {
+        assignmentElem = list.map((item, idx) => {
+            return(
+                <AssignmentCodeBox
+                    id={item.id}
+                    studentName={item.studentName}
+                    score={item.score}
+                    code={item.code}
+                    result={item.result}
+                    evaluationIdx={item.evaluationIdx}
+                ></AssignmentCodeBox>
+            );
+        })
+        /*
         assignmentElem = (
-            <AssignmentCodeBox></AssignmentCodeBox>
+            <AssignmentCodeBox
+                id={assignment.id}
+                studentName={assignment.studentName}
+                score={assignment.score}
+                code={assignment.code}
+                result={assignment.result}
+                evaluationIdx={assignment.evaluationIdx}
+            ></AssignmentCodeBox>
         );
+        */
     }
     return (
         <MainLayout>
@@ -30,4 +99,4 @@ const AssignmentEvaluation = (props) => {
     );
 };
 
-export default AssignmentEvaluation;
+export default inject("storeMain", "storeLecture", "storeTask")(observer(AssignmentEvaluation));
