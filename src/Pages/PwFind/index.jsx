34667743905e29c78822f2ab459dd0118c5b577c
hyunju <aphj2309@gmail.com>
2020-06-07 21:19:18 +0900

    #35 비밀번호 찾기 서버연동

--- src/Pages/PwFind/index.jsx
+++ src/Pages/PwFind/index.jsx
@@ -1,12 +1,42 @@
 import React, { useEffect, useState, useRef, memo } from "react"; 
- 
+import { observer, inject } from "mobx-react";
+
 import Input from "@components/Input"; 
 import Button from "@components/Button"; 
 import MainLayout from "@templates/MainLayout"; 
- 
+
+import * as Util from "@util";
 import "./style.scss"; 
  
 const PwFind = (props) => { 
+
+    const { storeMain } = props;
+    const [id, setId] = useState("");
+    const [email, setEmail] = useState("");
+
+    const handleIdChange = (e) => {
+        setId(e.target.value);
+    };
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const findBtn = (e) => {
+        Util.requestServer("auth/find/pw", "POST", {
+            id: id,
+            email: email,
+        }).then(async function (result) {
+            if (result.code == 200) {
+               
+                alert(result.body.msg);
+                
+                props.history.push("/login");
+            } else {
+                alert(result.body.msg);
+            }
+        });
+    };
  
     return ( 
         <MainLayout> 
@@ -16,7 +46,9 @@ const PwFind = (props) => {
                     <div className="inputs"> 
                         <Input 
                             type="text" 
-                            placeholder="아이디(학번)" 
+                            placeholder="아이디(학번)"
+                            value={id}
+                            onChange={handleIdChange}
                             margin="0px 0px 10px 0px" 
                             width="100%" 
                         ></Input> 
@@ -24,12 +56,14 @@ const PwFind = (props) => {
                         <Input 
                             type="email" 
                             placeholder="이메일" 
+                            value={email}
+                            onChange={handleEmailChange}
                             margin="0px 0px 10px 0px" 
                             width="100%" 
                         ></Input> 
                     </div> 
                     <div className="boxBottom"> 
-                        <Button value="비밀번호 메일 보내기" height="50px"></Button> 
+                        <Button value="비밀번호 메일 보내기" height="50px" onClick={findBtn}></Button> 
                     </div> 
                 </div> 
             </div> 
@@ -37,4 +71,4 @@ const PwFind = (props) => {
     ); 
 }; 
  
-export default PwFind; 
+export default inject("storeMain")(memo(PwFind)); 
